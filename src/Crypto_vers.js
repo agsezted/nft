daea15d1fcf6f4fc39358b72c2a6aa32f6cb7552
AGSEZTED <93152344+agsezted@users.noreply.github.com>
2022-02-15 12:03:56 -0400

    Add files via upload

---
+++ src/Crypto_vers.js
@@ -0,0 +1,264 @@
+{
+  CONTRACT_ADDRESS: "0x2aA884096f791cdAaE6F0EA5f8cAB1cF45956A0A",
+  SCAN_LINK= "https://testnet.bscscan.com/address/0x2aA884096f791cdAaE6F0EA5f8cAB1cF45956A0A",
+  NETWORK_ID= {
+    NAME: "bsctestnet",
+    SYMBOL: "BNB",
+    ID: 97
+  },
+
+  NFT_NAME= "Cryptovers NFT",
+  SYMBOL= "CVNFT",
+  MAX_SUPPLY= 200000,
+  WEI_COST= 0.0125 * 10 ** 18,
+  DISPLAY_COST= 0.0125,
+  GAS_LIMIT= 285000
+}
+
+var NFT_PRICE = null
+var PRESALE_PRICE = null
+var MAX_SUPPLY = null
+var MAX_PRESALE_SUPPLY = null
+var contract
+var accounts
+var web3
+var balance
+var available
+
+function metamaskReloadCallback()
+{
+  window.ethereum.on('accountsChanged', (accounts) => {
+    document.getElementById("web3_message").textContent="Accounts changed, realoading...";
+    window.location.reload()
+  })
+  window.ethereum.on('networkChanged', (accounts) => {
+    document.getElementById("web3_message").textContent="Network changed, realoading...";
+    window.location.reload()
+  })
+}
+
+const getAccounts = async () => {
+  metamaskReloadCallback()
+  try {
+    await window.ethereum.request({ method: "eth_requestAccounts" })
+    resolve(web3)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getWeb3 = async () => {
+  return new Promise((resolve, reject) => {
+    if(document.readyState=="complete")
+    {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum)
+        resolve(web3)
+      } else {
+        reject("must install MetaMask")
+        document.getElementById("web3_message").textContent="Error: Please install Metamask";
+      }
+    }else
+    {
+      window.addEventListener("load", async () => {
+        if (window.ethereum) {
+          const web3 = new Web3(window.ethereum)
+          resolve(web3)
+        } else {
+          reject("must install MetaMask")
+          document.getElementById("web3_message").textContent="Error: Please install Metamask";
+        }
+      });
+    }
+  });
+};
+
+function handleRevertError(message) {
+  alert(message)
+}
+
+async function getRevertReason(txHash) {
+  const tx = await web3.eth.getTransaction(txHash)
+  await web3.eth
+    .call(tx, tx.blockNumber)
+    .then((result) => {
+      throw Error("unlikely to happen")
+    })
+    .catch((revertReason) => {
+      var str = "" + revertReason
+      json_reason = JSON.parse(str.substring(str.indexOf("{")))
+      handleRevertError(json_reason.message)
+    });
+}
+
+const getContract = async (web3) => {
+  const response = await fetch("client/contracts/abi.js");
+  const data = await response.json();
+
+  const netId = await web3.eth.net.getId();
+  const deployedNetwork = data.networks[netId];
+  contract = new web3.eth.Contract(
+    data.abi,
+    deployedNetwork && deployedNetwork.address
+    );
+  return contract
+}
+
+async function connectWallet() {
+  getAccounts()
+}
+
+async function loadAccount() {
+  accounts = await web3.eth.getAccounts()
+  balance = await contract.methods.balanceOf(accounts[0]).call()
+  document.getElementById("web3_message").textContent="Connected"
+  document.getElementById("connect_button").style.display = "none"
+  document.getElementById("nft_balance").textContent="You have " + balance + "CRYPTOVERSNFT"
+}
+
+async function loadDapp() {
+  document.getElementById("web3_message").textContent="Connecting..."
+  var awaitWeb3 = async function () {
+    web3 = await getWeb3()
+    web3.eth.net.getId((err, netId) => {
+      if (netId == NETWORK_ID) {
+        var awaitContract = async function () {
+          contract = await getContract(web3);
+          NFT_PRICE = await contract.methods.price().call()
+          MAX_SUPPLY = await contract.methods.MAX_SUPPLY().call()
+          MAX_PRESALE_SUPPLY = await contract.methods.MAX_PRESALE_SUPPLY().call()
+          total_mint = await contract.methods.totalSupply().call()
+          available = MAX_SUPPLY - total_mint
+          available_presale = MAX_PRESALE_SUPPLY - total_mint
+          if(document.getElementById("total_mint"))
+            document.getElementById("total_mint").textContent = available + "/" + MAX_SUPPLY + " available"
+          if(document.getElementById("total_mint_presale"))
+            document.getElementById("total_mint_presale").textContent = available_presale + "/" + MAX_PRESALE_SUPPLY + " available"
+          if(document.getElementById("price"))
+            document.getElementById("price").textContent= "Price: " + web3.utils.fromWei(NFT_PRICE) + " BNB"
+          if(document.getElementById("presale_price"))
+            document.getElementById("presale_price").textContent= "Presale Price: " + web3.utils.fromWei(PRESALE_PRICE) + " BNB"
+          web3.eth.getAccounts(function(err, accounts){
+            if (err != null)
+              console.error("An error occurred: "+err);
+            else if (accounts.length == 0)
+              console.log("User is not logged in to MetaMask");
+            else
+            {
+              loadAccount()
+            }
+          });
+        };
+        awaitContract();
+      } else {
+        document.getElementById("web3_message").textContent="Please connect to bsc testnet";
+      }
+    });
+  };
+  awaitWeb3();
+}
+
+loadDapp()
+
+document.getElementById("web3_message").textContent="Please connect to Metamask"
+
+/* SALE */
+
+const mint = async () => {
+  let mint_amount = document.getElementById("mint_amount").value
+  const result = await contract.methods.mintToken(mint_amount)
+    .send({ from: accounts[0], gas: 0, value: NFT_PRICE * mint_amount })
+    .on('transactionHash', function(hash){
+      document.getElementById("web3_message").textContent="Minting...";
+    })
+    .on('receipt', function(receipt){
+      document.getElementById("web3_message").textContent="Success! Minting finished.";    })
+    .catch((revertReason) => {
+      getRevertReason(revertReason.receipt.transactionHash);
+    });
+}
+
+/* PRESALE */
+
+const mintPresale = async () => {
+  let mint_amount = document.getElementById("mint_amount").value
+  const result = await contract.methods.mintPresale(mint_amount)
+    .send({ from: accounts[0], gas: 0, value: PRESALE_PRICE * mint_amount })
+    .on('transactionHash', function(hash){
+      document.getElementById("web3_message").textContent="Minting...";
+    })
+    .on('receipt', function(receipt){
+      document.getElementById("web3_message").textContent="Success! Minting finished.";    })
+    .catch((revertReason) => {
+      getRevertReason(revertReason.receipt.transactionHash);
+    });
+}
+
+/* Owner */
+
+const mintReserved = async () => {
+  let mint_amount = document.getElementById("mint_amount").value
+  const result = await contract.methods.mintReserved(mint_amount)
+    .send({ from: accounts[0], gas: 0, value: 0 })
+    .on('transactionHash', function(hash){
+      document.getElementById("web3_message").textContent="Minting...";
+    })
+    .on('receipt', function(receipt){
+      document.getElementById("web3_message").textContent="Success! Minting finished.";    })
+    .catch((revertReason) => {
+      getRevertReason(revertReason.receipt.transactionHash);
+    });
+}
+
+const setPresaleActive = async () => {
+  const result = await contract.methods.setPresaleActive(true)
+    .send({ from: accounts[0], gas: 0, value: 0 })
+    .on('transactionHash', function(hash){
+      document.getElementById("web3_message").textContent="Minting...";
+    })
+    .on('receipt', function(receipt){
+      document.getElementById("web3_message").textContent="Success! Minting finished.";    })
+    .catch((revertReason) => {
+      getRevertReason(revertReason.receipt.transactionHash);
+    });
+}
+
+const setSaleActive = async () => {
+  const result = await contract.methods.setSaleActive(true)
+    .send({ from: accounts[0], gas: 0, value: 0 })
+    .on('transactionHash', function(hash){
+      document.getElementById("web3_message").textContent="Minting...";
+    })
+    .on('receipt', function(receipt){
+      document.getElementById("web3_message").textContent="Success! Minting finished.";    })
+    .catch((revertReason) => {
+      getRevertReason(revertReason.receipt.transactionHash);
+    });
+}
+
+const setBaseURI = async () => {
+  const result = await contract.methods.setBaseURI("http://")
+    .send({ from: accounts[0], gas: 0, value: 0 })
+    .on('transactionHash', function(hash){
+      document.getElementById("web3_message").textContent="Minting...";
+    })
+    .on('receipt', function(receipt){
+      document.getElementById("web3_message").textContent="Success! Minting finished.";    })
+    .catch((revertReason) => {
+      getRevertReason(revertReason.receipt.transactionHash);
+    });
+}
+
+const setPrice = async () => {
+  const result = await contract.methods.setPrice("0")//(10000000)
+    .send({ from: accounts[0], gas: 0, value: 0 })
+    .on('transactionHash', function(hash){
+      document.getElementById("web3_message").textContent="Minting...";
+    })
+    .on('receipt', function(receipt){
+      document.getElementById("web3_message").textContent="Success! Minting finished.";    })
+    .catch((revertReason) => {
+      getRevertReason(revertReason.receipt.transactionHash);
+    });
+}
+
