79d8f172bf9baa1b20b7c82f37696c80ed895a01
AGSEZTED <93152344+agsezted@users.noreply.github.com>
2022-02-25 18:45:01 -0400

    Update cryptoversABI.js

--- paginas/cryptoversABI.js
+++ paginas/cryptoversABI.js
@@ -207,19 +207,6 @@
 		"type": "function"
 	},
 	{
-		"inputs": [],
-		"name": "initial_price",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
 		"inputs": [
 			{
 				"internalType": "address",
